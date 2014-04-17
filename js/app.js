317d44b84782b4fad55d5d0ad5490dc45a87fd65
Mte90 <mte90smanettator@gmail.com>
2014-04-17 19:55:58 +0200

    popup code added

--- js/app.js
+++ js/app.js
@@ -2,22 +2,33 @@ document.addEventListener("DOMContentLoaded", function() {
   [].forEach.call(
           document.querySelectorAll('.content'),
           function(el) {
-            update_code(el);
             if (el.classList[0] !== 'home') {
+              update_code(el);
+              //Add the button for open the poup
               var e = document.createElement('button');
               e.classList.add('popup');
-              e.innerHTML = 'Open popup';
+              e.innerHTML = 'See in a popup';
               document.querySelector('.' + el.classList[0] + ' .mobile-view').parentNode.insertBefore(e, document.querySelector('.' + el.classList[0] + ' .mobile-view').parentNode.firstChild);
+              //Create the popup
+              e.addEventListener('click', function() {
+                var w = window.open('', "", "width=320, height=480, scrollbars=no");
+                w.document.body.innerHTML += '<link rel="stylesheet" type="text/css" href="http://' + window.location.hostname + window.location.pathname + '/css/brick-1.0.1.min.css">';
+                w.document.body.innerHTML += '<script type="text/javascript" src="http://' + window.location.hostname + window.location.pathname + '/js/brick-1.0.1.min.js"></script>';
+                w.document.body.innerHTML += document.querySelector('.' + el.classList[0] + ' .mobile-view').innerHTML;
+              });
+              el.addEventListener('change', function() {
+                update_code(el);
+              });
             }
-            el.addEventListener('change', function() {
-              update_code(el);
-            });
           }
   );
+  //Update the code
   function update_code(el) {
     if (el.classList[0] !== 'home') {
       document.querySelector('.' + el.classList[0] + ' .html-code').textContent = document.querySelector('.' + el.classList[0] + ' .mobile-view').innerHTML;
       Prism.highlightAll();
     }
   }
+
+
 });
